ba9967f2d46cd1f735db0e59b29422760510aa34
Impala2022 <gudipaatihareesh@gmail.com>
2022-06-30 20:36:45 +0530

    Producer Changes

--- employee/src/main/java/com/neosoft/employee/service/EmployeeService.java
+++ employee/src/main/java/com/neosoft/employee/service/EmployeeService.java
@@ -1,9 +1,14 @@
 package com.neosoft.employee.service;
 
 import com.neosoft.employee.entity.Employee;
+import com.neosoft.employee.kafka.CallBack;
+import com.neosoft.employee.kafka.EmployeeProducer;
 import com.neosoft.employee.repo.EmployeeRepoImpl;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class EmployeeService {
@@ -11,6 +16,8 @@ public class EmployeeService {
     public EmployeeRepoImpl employeeRepo;
     public int create(Employee details){
         employeeRepo.save(details);
+        EmployeeProducer kafkaProducer=new EmployeeProducer("Employee",details.toString());
+        kafkaProducer.start();
         return details.getEmpId();
     }
 }
